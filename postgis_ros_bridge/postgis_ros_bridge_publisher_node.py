a4d1e1668a054279383f4fcdede019e42a735045
Marco Wallner <marco.wallner@ait.ac.at>
2023-06-05 17:25:00 +0200

    applied code formatting

--- postgis_ros_bridge/postgis_ros_bridge_publisher_node.py
+++ postgis_ros_bridge/postgis_ros_bridge_publisher_node.py
@@ -5,7 +5,7 @@ import rclpy
 from builtin_interfaces.msg import Time
 from rcl_interfaces.msg import ParameterDescriptor, ParameterType
 from rclpy.node import Node
-from rclpy.publisher import  Publisher
+from rclpy.publisher import Publisher
 from rclpy.parameter import Parameter
 from std_msgs.msg import Header
 from geometry_msgs.msg import Point, PointStamped
@@ -27,7 +27,7 @@ class PostGisConverter:
     def to_point_tuple(geometry):
         point = wkb.loads(geometry, hex=True)
         return (point.x, point.y, point.z)
-    
+
 
 class PostgreSQLConnection(AbstractContextManager):
     def __init__(self, node: Node):
@@ -50,36 +50,37 @@ class PostgreSQLConnection(AbstractContextManager):
         schema = node.get_parameter(f"{ns}.schema").value
 
         connection_uri = f"postgresql://{user}:{passwd}@{host}:{port}/{schema}"
-        self.engine = create_engine(connection_uri, execution_options={'postgresql_readonly': True})
-    
+        self.engine = create_engine(connection_uri, execution_options={
+                                    'postgresql_readonly': True})
+
     def __enter__(self):
         return self
-    
+
     def __exit__(self):
         self.engine.dispose()
 
     def __repr__(self) -> str:
         return super().__repr__() + f"({self.engine})"
-    
+
 
 class Query(AbstractContextManager):
     def __init__(self, postgresql_conn: PostgreSQLConnection, query: str):
         Session = sessionmaker(bind=postgresql_conn.engine)
         self._session = Session()
         self._query = text(query)
-        
+
     def __enter__(self):
         return self
-    
+
     def __exit__(self):
         self._session.close()
 
     def get_results(self) -> Result:
         return self._session.execute(self._query)
-    
+
     def __repr__(self) -> str:
         return super().__repr__() + f"({self._query})"
-    
+
 
 class QueryResultParser(ABC):
     TYPE = "AbstractParser"
@@ -87,7 +88,6 @@ class QueryResultParser(ABC):
     @abstractmethod
     def declare_params(self) -> Iterable[Tuple[str, Any, ParameterDescriptor]]:
         return []
-    
 
     @abstractmethod
     def set_params(self, params: Dict[str, Parameter]) -> Iterable[Tuple[str, Any]]:
@@ -96,10 +96,9 @@ class QueryResultParser(ABC):
     @abstractmethod
     def parse_result(self, result: Result, time: Time) -> Iterable[Tuple[str, Any]]:
         return []
-    
+
     def __repr__(self) -> str:
         return self.TYPE
-
 
 
 class PointResultParser(QueryResultParser):
@@ -107,29 +106,31 @@ class PointResultParser(QueryResultParser):
 
     def __init__(self):
         pass
-    
+
     def declare_params(self) -> Iterable[Tuple[str, Any, ParameterDescriptor]]:
         return [
-            ('frame_id', '', ParameterDescriptor(name='frame_id', type=ParameterType.PARAMETER_STRING)),
-            ('topic', 'point', ParameterDescriptor(name='topic', type=ParameterType.PARAMETER_STRING))
+            ('frame_id', '', ParameterDescriptor(
+                name='frame_id', type=ParameterType.PARAMETER_STRING)),
+            ('topic', 'point', ParameterDescriptor(
+                name='topic', type=ParameterType.PARAMETER_STRING))
         ]
-
 
     def set_params(self, params: Dict[str, Parameter]) -> Iterable[Tuple[str, Any]]:
         self.frame_id = params['frame_id'].value
         self.topic = params['topic'].value
         return [(self.topic, PointStamped)]
 
-
     def parse_result(self, result: Result, time: Time) -> Iterable[Tuple[str, Any]]:
         def get_frame_id(elem):
             return self.frame_id if self.frame_id else elem.frame_id if hasattr(elem, 'frame_id') else 'map'
-        return ((self.topic, PointStamped(header=Header(frame_id=get_frame_id(element), stamp=time), point=PostGisConverter.to_point(element.geometry))) for element in result) 
-    
+        return ((self.topic, 
+                 PointStamped(header=Header(frame_id=get_frame_id(element), stamp=time), 
+                              point=PostGisConverter.to_point(element.geometry))) for element in result)
+
     def __repr__(self) -> str:
         return super().__repr__() + f"({self.frame_id}, {self.topic})"
 
-    
+
 class PC2ResultParser(QueryResultParser):
     TYPE = "PointCloud2"
 
@@ -138,15 +139,17 @@ class PC2ResultParser(QueryResultParser):
 
     def declare_params(self) -> Iterable[Tuple[str, Any, ParameterDescriptor]]:
         return [
-            ('frame_id', '', ParameterDescriptor(name='frame_id', type=ParameterType.PARAMETER_STRING)),
-            ('topic', 'point', ParameterDescriptor(name='topic', type=ParameterType.PARAMETER_STRING))
+            ('frame_id', '', ParameterDescriptor(
+                name='frame_id', type=ParameterType.PARAMETER_STRING)),
+            ('topic', 'point', ParameterDescriptor(
+                name='topic', type=ParameterType.PARAMETER_STRING))
         ]
-    
+
     def set_params(self, params: Dict[str, Parameter]) -> Iterable[Tuple[str, Any]]:
         self.frame_id = params['frame_id'].value
         self.topic = params['topic'].value
         return [(self.topic, PointCloud2)]
-    
+
     def parse_result(self, result: Result, time: Time) -> Iterable[Tuple[str, Any]]:
         def get_frame_id():
             return self.frame_id if self.frame_id else 'map'
@@ -157,10 +160,9 @@ class PC2ResultParser(QueryResultParser):
         ]
         pointcloud_msg = point_cloud2.create_cloud_xyz32(header, points)
         return [(self.topic, pointcloud_msg)]
-    
+
     def __repr__(self) -> str:
         return super().__repr__() + f"({self.frame_id}, {self.topic})"
-
 
 
 class PostGisConverter:
@@ -249,8 +251,10 @@ class PostGisConverter:
 #     def __str__(self):
 #         return f'{self.__class__.__name__}: \n query: "{self.query}" \b topic: {self.topic}, \n frame_id: {self.frame_id}'
 
-#TODO: Maybe extension points
-query_converter: Dict[str, QueryResultParser] = {q.TYPE: q for q in [PointResultParser, PC2ResultParser]}
+# TODO: Maybe extension points
+query_converter: Dict[str, QueryResultParser] = {
+    q.TYPE: q for q in [PointResultParser, PC2ResultParser]}
+
 
 class PostGisPublisher(Node):
     def __init__(self):
@@ -286,20 +290,23 @@ class PostGisPublisher(Node):
                 raise ValueError(
                     f"Type: '{query_type}' is not supported. Supported: {query_converter.keys()}"
                 )
-            
+
             converter = query_converter[query_type]()
-            self.declare_parameters(namespace=config, parameters=converter.declare_params())
-            topics_msgs = converter.set_params(self.get_parameters_by_prefix(config))
+            self.declare_parameters(
+                namespace=config, parameters=converter.declare_params())
+            topics_msgs = converter.set_params(
+                self.get_parameters_by_prefix(config))
             query = Query(self.postgresql_connection, sql_query)
 
             # TODO: probably sensor data qos or later configurable
-            pubs = [(t, self.create_publisher(m, t, 10)) for (t, m) in topics_msgs]
+            pubs = [(t, self.create_publisher(m, t, 10))
+                    for (t, m) in topics_msgs]
             self.converter_pubs.update(pubs)
 
-            self.create_timer(1.0/rate, partial(self.timer_callback, query, converter))
+            self.create_timer(
+                1.0/rate, partial(self.timer_callback, query, converter))
 
             self.get_logger().info(str(converter))
-
 
     def timer_callback(self, query: Query, converter: QueryResultParser):
         for t, m in converter.parse_result(query.get_results(), self.get_clock().now().to_msg()):
