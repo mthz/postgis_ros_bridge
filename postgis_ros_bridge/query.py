8c189b98b72a92080b4bb2cf9fab9d0b7f0eda7b
Marco Wallner <marco.wallner@ait.ac.at>
2023-06-14 11:30:19 +0200

    applied linting to query

--- postgis_ros_bridge/query.py
+++ postgis_ros_bridge/query.py
@@ -1,23 +1,28 @@
+"""Query class for executing SQL queries on the database."""
 from contextlib import AbstractContextManager
 
-from postgis_ros_bridge.postgresql_connection import PostgreSQLConnection
 from sqlalchemy import Result, text
 from sqlalchemy.orm import sessionmaker
 
+from postgis_ros_bridge.postgresql_connection import PostgreSQLConnection
+
 
 class Query(AbstractContextManager):
+    """Query class for executing SQL queries on the database."""
+
     def __init__(self, postgresql_conn: PostgreSQLConnection, query: str):
-        Session = sessionmaker(bind=postgresql_conn.engine)
-        self._session = Session()
+        session_ = sessionmaker(bind=postgresql_conn.engine)
+        self._session = session_()
         self._query = text(query)
 
     def __enter__(self):
         return self
 
-    def __exit__(self):
+    def __exit__(self, _type, _value, _traceback):
         self._session.close()
 
     def get_results(self) -> Result:
+        """Execute the query and return the results."""
         return self._session.execute(self._query)
 
     def __repr__(self) -> str:
